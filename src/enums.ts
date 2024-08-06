da64df3b416bc2962d8ae40726ce8aeeca8d8248
Agustin <agustingonzaleztam@gmail.com>
2024-08-06 04:56:40 -0300

    added enums

---
+++ src/enums.ts
@@ -0,0 +1,10 @@
+export enum SearchModeEnum {
+  REAL_TIME = "real-time",
+  HISTORICAL = "historical",
+}
+
+export enum IntervalEnum {
+  MIN_1 = "1min",
+  MIN_5 = "5min",
+  MIN_15 = "15min",
+}
